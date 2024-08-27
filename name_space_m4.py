8cd695ba1600a170a6de66b79f95c6da89e702fa
binraven <tihonov-ks@ya.ru>
2024-08-27 12:07:28 +0500

    Add files via upload

---
+++ name_space_m4.py
@@ -0,0 +1,12 @@
+def test_function(*args):
+    def inner_function (*args):
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+test_function()
+# inner_function()
+# Traceback (most recent call last):
+#   File "D:\MyPyLessons\Lessons\name_space_m4.py", line 7, in <module>
+#     inner_function()
+#     ^^^^^^^^^^^^^^
+# NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
