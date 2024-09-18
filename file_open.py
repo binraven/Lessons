82c4dd08ff0993498df79ec47154708033260f00
binraven <tihonov-ks@ya.ru>
2024-09-18 11:29:15 +0500

    Update file_open.py

--- file_open.py
+++ file_open.py
@@ -26,12 +26,11 @@ class Shop:
                 file.close()
 
     
-class Product (Shop):
+class Product ():
     def __init__(self, name, weight, category):
         self.name = name
         self.weight = weight
-        self.category = category
-        #super().add(name, weight, category)
+        self.category = category        
 
     def __str__(self):
         return f"{self.name}, {self.weight}, {self.category}"
