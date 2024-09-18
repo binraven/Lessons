f8ab6ea66d1ea3c3aaa554876cab59dfc2078c2b
binraven <tihonov-ks@ya.ru>
2024-09-18 11:26:49 +0500

    Add files via upload

---
+++ file_open.py
@@ -0,0 +1,57 @@
+"""
+Цель: закрепить знания о работе с файлами (чтение/запись) решив задачу.
+
+Задача "Учёт товаров":
+
+#module_7_1.py
+"""
+
+
+class Shop:
+    __file_name = "products.txt"
+
+    def get_products(self):
+        file = open(self.__file_name, "r")
+        file_product = file.read()
+        file.close()
+        return file_product
+    def add(self, *products):
+        file_product = self.get_products()
+        for i in products:
+            if str(i) in file_product:
+                print(f"Продукт {i} уже есть в магазине")
+            else:
+                file = open(self.__file_name, "a")
+                file.write(f"{str(i)}\n" )
+                file.close()
+
+
+
+        #
+        # str_write = ", ".join(map(str, products))
+        # if products[0] in file_product:
+        #     print(f"Продукт {str_write} уже есть в магазине")
+        # else:
+        #     file = open(self.__file_name, "a")
+        #     file.write(f"{str_write}\n" )
+        #     file.close()
+class Product (Shop):
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+        #super().add(name, weight, category)
+
+    def __str__(self):
+        return f"{self.name}, {self.weight}, {self.category}"
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
