f2d964b290beecf186ab7b6415b31c0be36ab74b
binraven <tihonov-ks@ya.ru>
2024-09-19 10:54:56 +0500

    Update file_oper_with.py

--- file_oper_with.py
+++ file_oper_with.py
@@ -47,32 +47,3 @@ finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words()) # Все слова
 print(finder2.find('TEXT')) # 3 слово по счёту
 print(finder2.count('teXT')) # 4 слова teXT в тексте всего
-
-
-
-# name = ("test_file.txt",)
-# exclude = [',', '.', '=', '!', '?', ';', ':', ' - ','\n']
-# get_all_words = {}
-# find_word = "TexT"
-# find_dict = {}
-#
-# for i in name:
-#     str_name = i
-#     with open(str_name, encoding="utf-8") as file:
-#         get_all_words_list = []
-#         for line in file:
-#             s = ''.join(ch for ch in line if ch not in exclude)
-#             get_all_words_list.extend(s.lower().split(" "))
-#
-#
-#     get_all_words[str_name] = get_all_words_list
-#
-# for key, value in get_all_words.items():
-#     for i in range (len(value)):
-#         if find_word.lower() == value[i]:
-#             find_dict[key] = i+1
-#             break
-#
-# print(find_dict)
-#
-# #print(get_all_words)
