b23f093c0b9940bb8e2434a8ba4faf03ebed99d0
binraven <tihonov-ks@ya.ru>
2024-09-19 14:58:25 +0500

    Update file_os.py

--- file_os.py
+++ file_os.py
@@ -4,9 +4,12 @@ import time
 directory = "."
 
 
-for root, dirs, files in os.walk(directory):
-    for file in files:
-
-        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(os.stat(file).st_mtime))
-        print(f"Обнаружен файл: {file}, Путь: {os.path.join(root, file)}, Размер: {os.stat(file).st_size} байт, "
-              f"Время изменения: {formatted_time}, Родительская директория {root} ")
+for i in os.walk(directory):
+    file_list = i[2]
+    abs_path = i[0]
+    for file in file_list:
+      formatted_time = time.strftime("%d.%m.%Y %H:%M",
+                                     time.localtime(os.stat(os.path.join(abs_path, file)).st_mtime))
+      print(f"Обнаружен файл: {file}, Путь: {os.path.join(abs_path, file)}, "
+            f"Размер: {os.stat(os.path.join(abs_path, file)).st_size} байт, "
+            f"Время изменения: {formatted_time}, Родительская директория {abs_path} ")
