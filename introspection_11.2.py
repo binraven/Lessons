7a6e90ea0f366647b7d9b7b02e9a677b730e0b45
binraven <tihonov-ks@ya.ru>
2024-10-31 10:28:17 +0500

    Update introspection_11.2.py

--- introspection_11.2.py
+++ introspection_11.2.py
@@ -3,7 +3,16 @@ from pprint import pprint
 
 
 def introspection_info(obj):
-    dict_info = {"type": type(obj), 'attributes': dir(obj),  'module': inspect.getmodule(obj)}
+    obj_attr_list = []
+    obj_method_list = []
+    for attr_name in dir(obj):
+        attr = getattr(obj, attr_name)
+        if inspect.isbuiltin(attr) or inspect.ismethod(attr) or inspect.isfunction(attr):
+            obj_method_list.append(attr_name)
+        else:
+            obj_attr_list.append(attr_name)
+
+    dict_info = {"type": type(obj), 'attributes': obj_attr_list, 'methods': obj_method_list, 'module': inspect.getmodule(obj)}
     return dict_info
 
 
