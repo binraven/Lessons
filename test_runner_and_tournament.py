a50edc9f07e5435ce0b8bc12bc86d2f8d53abeb6
binraven <tihonov-ks@ya.ru>
2024-11-13 19:59:05 +0500

    Update test_runner_and_tournament.py

--- test_runner_and_tournament.py
+++ test_runner_and_tournament.py
@@ -1,6 +1,7 @@
 import runner_and_tournament
 import unittest
 
+
 class TournamentTest(unittest.TestCase):
 
     def setUp(self):
@@ -8,33 +9,36 @@ class TournamentTest(unittest.TestCase):
         self.runner2 = runner_and_tournament.Runner("Андрей", 9)
         self.runner3 = runner_and_tournament.Runner("Ник", 3)
 
-
     @classmethod
     def setUpClass(cls):
-        TournamentTest.all_results = []
+        cls.all_results = {}
 
     @classmethod
     def tearDownClass(cls):
         temp_dic = {}
-        for item in TournamentTest.all_results:
-            for x, y in item.items():
-                temp_dic[x] = y.name
+        for x in cls.all_results:
+            for key, value in cls.all_results[x].items():
+                temp_dic[key] = value.name
             print(temp_dic)
-
-
 
     def test_tournament1(self):
         t1 = runner_and_tournament.Tournament(90, self.runner1, self.runner3)
-        TournamentTest.all_results.append(t1.start())
+        self.all_results[max(self.id().split("."))] = t1.start()
+        self.assertTrue("Ник" == self.all_results[max(self.id().split("."))]
+        [max(self.all_results[max(self.id().split("."))].keys())])
 
     def test_tournament2(self):
         t2 = runner_and_tournament.Tournament(90, self.runner2, self.runner3)
-        TournamentTest.all_results.append(t2.start())
+        self.all_results[max(self.id().split("."))] = t2.start()
+        self.assertTrue("Ник" == self.all_results[max(self.id().split("."))]
+        [max(self.all_results[max(self.id().split("."))].keys())])
 
     def test_tournament3(self):
-        t2 = runner_and_tournament.Tournament(90, self.runner1, self.runner2, self.runner3)
-        TournamentTest.all_results.append(t2.start())
+        t3 = runner_and_tournament.Tournament(90, self.runner1, self.runner2, self.runner3)
+        self.all_results[max(self.id().split("."))] = t3.start()
+        self.assertTrue("Ник" == self.all_results[max(self.id().split("."))]
+        [max(self.all_results[max(self.id().split("."))].keys())])
+
 
 if __name__ == "__main__":
     unittest.main()
-
