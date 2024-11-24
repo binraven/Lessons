98ec2a46baaf3108bc3f367f3dfb32f5d7e222a2
binraven <tihonov-ks@ya.ru>
2024-11-24 14:40:13 +0500

    Add files via upload

---
+++ bot_13_3.py
@@ -0,0 +1,20 @@
+import asyncio
+import conf_bot
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+bot = Bot(token=conf_bot.TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=["start"])
+async def start_message(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
