e74954d45b991dc72d9fc4f85fb9e3d1b0538223
binraven <tihonov-ks@ya.ru>
2024-12-23 23:27:21 +0500

    Update curse_16_3.py

--- curse_16_3.py
+++ curse_16_3.py
@@ -4,9 +4,11 @@ from typing import Annotated, Dict
 app = FastAPI()
 users = {'1': 'Имя: Example, возраст: 18'}
 
+
 @app.get("/")
 async def all_users() -> dict:
     return users
+
 
 @app.delete("/user/{user_id}")
 async def read_user_id(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="123")]) -> str:
@@ -17,24 +19,23 @@ async def read_user_id(user_id: Annotated[int, Path(ge=1, le=100, description="E
         return f"The user id {user_id} wasn't deleted (not found)"
 
 
-
-
-
 @app.post("/user/{username}/{age}")
-async def create_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
-                         , age: int = Path(ge=18, le=120, description="Enter age'", example="24")) -> str:
+async def create_user(
+        username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age'", example="24")]) -> str:
     user_id = str(int(max(users, key=int)) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
 
+
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
-                         , age: int = Path(ge=18, le=120, description="Enter age'", example="24"),
-                      user_id: int = Path(ge=1, le=100, description="Enter User ID", example="123")) -> str:
+async def update_user(
+        username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age'", example="24")],
+        user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="123")]) -> str:
     for user in users.keys():
-        if user == user_id:
+        if str(user) == str(user_id):
             users[user_id] = f"Имя: {username}, возраст: {age}"
             return f"The user id {user_id} is updated"
         else:
             return f"The user id {user_id} not updated (not found)"
-
