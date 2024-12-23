9a5bed9b671b692b8e1c58dbb2bb2a1e8f33b05c
binraven <tihonov-ks@ya.ru>
2024-12-23 12:48:46 +0500

    Add files via upload

---
+++ curse_16_3.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Path
+from typing import Annotated, Dict
+
+app = FastAPI()
+users = {'1': 'Имя: Example, возраст: 18'}
+
+@app.get("/")
+async def all_users() -> dict:
+    return users
+
+@app.delete("/user/{user_id}")
+async def read_user_id(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="123")]) -> str:
+    users.pop(str(user_id))
+    return f"The user {user_id} was deleted"
+
+
+@app.post("/user/{username}/{age}")
+async def create_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
+                         , age: int = Path(ge=18, le=120, description="Enter age'", example="24")) -> str:
+    user_id = str(int(max(users, key=int)) + 1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} is registered"
+
+@app.put("/user/{user_id}/{username}/{age}")
+async def create_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
+                         , age: int = Path(ge=18, le=120, description="Enter age'", example="24"),
+                      user_id: int = Path(ge=1, le=100, description="Enter User ID", example="123")) -> str:
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"The user {user_id} is updated"
+
