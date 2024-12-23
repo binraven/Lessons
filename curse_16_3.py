2269234b613c0f40965eeda6685f32b739a886af
binraven <tihonov-ks@ya.ru>
2024-12-23 16:25:10 +0500

    Update curse_16_3.py

--- curse_16_3.py
+++ curse_16_3.py
@@ -10,8 +10,14 @@ async def all_users() -> dict:
 
 @app.delete("/user/{user_id}")
 async def read_user_id(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="123")]) -> str:
-    users.pop(str(user_id))
-    return f"The user {user_id} was deleted"
+    try:
+        users.pop(str(user_id))
+        return f"The user id {user_id} was deleted"
+    except Exception as e:
+        return f"The user id {user_id} wasn't deleted (not found)"
+
+
+
 
 
 @app.post("/user/{username}/{age}")
@@ -22,9 +28,13 @@ async def create_user(username: str = Path(min_length=5, max_length=20, descript
     return f"User {user_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def create_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
+async def update_user(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
                          , age: int = Path(ge=18, le=120, description="Enter age'", example="24"),
                       user_id: int = Path(ge=1, le=100, description="Enter User ID", example="123")) -> str:
-    users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"The user {user_id} is updated"
+    for user in users.keys():
+        if user == user_id:
+            users[user_id] = f"Имя: {username}, возраст: {age}"
+            return f"The user id {user_id} is updated"
+        else:
+            return f"The user id {user_id} not updated (not found)"
 
