867362dfbb1935f9125de288735a6658be205712
binraven <tihonov-ks@ya.ru>
2024-12-23 12:41:23 +0500

    Update curse_16_2.py

--- curse_16_2.py
+++ curse_16_2.py
@@ -4,20 +4,18 @@ from typing import Annotated
 app = FastAPI()
 
 @app.get("/")
-async def read_root() -> dict:
-    return {"message": "Главная страница"}
+async def read_root() -> str:
+    return "Главная страница"
 
 @app.get("/user/admin")
-async def read_admin() -> dict:
-    return {"message": "Вы вошли как администратор"}
+async def read_admin() -> str:
+    return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async def read_user_id(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="123")]) -> dict:
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async def read_user_id(user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="123")]) -> str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user/{username}/{age}")
 async def read_user_info(username: str = Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")
-                         , age: int = Path(ge=18, le=120, description="Enter age'", example="24")) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
-
-
+                         , age: int = Path(ge=18, le=120, description="Enter age'", example="24")) -> str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
